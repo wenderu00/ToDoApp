ecd3edda7a5f4bbec6b15069b896f636bdf2c762
wenderu00 <mwmcjr@gmail.com>
2022-02-08 15:06:34 -0300

    elaboração parcial do createtodocommand

---
+++ ToDo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,22 @@
+﻿]using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Commands
+{
+    public class CreateTodoCommand
+    {
+        public CreateTodoCommand() { }
+        public CreateTodoCommand(string title, string user, DateTime date) {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+        public string Title { get; set; }
+        public string User {get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}

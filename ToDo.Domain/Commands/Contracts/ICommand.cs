b3a8566d0af9c033050cca434344104e8677969c
wenderu00 <mwmcjr@gmail.com>
2022-02-08 14:57:54 -0300

    interface para commands futuros

--- ToDo.Domain/Commands/Contracts/ICommand.cs
+++ ToDo.Domain/Commands/Contracts/ICommand.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace ToDo.Domain.Commands.Contracts
 {
     public interface ICommand
     {
-        bool Validate();
+        void Validate();
     }
 }

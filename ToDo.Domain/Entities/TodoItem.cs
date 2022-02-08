e862013d5c71850cd5aa975553a7fed9a7c33d44
wenderu00 <mwmcjr@gmail.com>
2022-02-08 12:54:28 -0300

    Criação da entidade TodoItem e elaboração do construtor, metodos e propriedades.

---
+++ ToDo.Domain/Entities/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Entities
+{
+    internal class TodoItem : Entity
+    {
+        public TodoItem(string title,string user, DateTime date)
+        {
+            Title = title;
+            Done = false;
+            User = user;
+            Date = date;
+
+        }
+        public string Title { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+        public void MarkAsDone()
+        {
+            Done = true;
+        }
+        public void MarkAsUndone()
+        {
+            Done= false;
+        }
+        public void UpdateTitle(string title)
+        {
+            Title=title;
+        }
+    }
+}

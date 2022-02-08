cac42c6408c98727fe745bc9c6c7fd23a4deda7d
wenderu00 <mwmcjr@gmail.com>
2022-02-08 11:54:28 -0300

    criação de entidade base e metodo do comparação de id's

--- ToDo.Domain/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ToDo.Domain;
-public class Class1
-{
-
-}
---
+++ ToDo.Domain/Entities/Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Entities
+{
+    internal abstract class Entity : IEquatable<Entity>
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+
+        public bool Equals(Entity? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            return Id== other.Id;
+        }
+    }
+}

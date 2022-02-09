a045e3fc8a0219c78804aca9e983380c6fe85421
wenderu00 <mwmcjr@gmail.com>
2022-02-09 16:08:53 -0300

    concluindo repositorio

--- ToDo.Domain.Infra/Repositories/TodoRepository.cs
+++ ToDo.Domain.Infra/Repositories/TodoRepository.cs
@@ -1,23 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ToDo.Domain.Entities;
+using ToDo.Domain.Infra.Contexts;
+using ToDo.Domain.Queries;
 using ToDo.Domain.Repositories;
 
 namespace ToDo.Domain.Infra.Repositories
 {
     public class TodoRepository : ITodoRepository
     {
+        private readonly TodoContext _context;
+        public TodoRepository(TodoContext context) { _context = context; }
         public void Create(TodoItem todo)
         {
-            throw new NotImplementedException();
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
         }
 
         public IEnumerable<TodoItem> GetAll(string user)
         {
-            throw new NotImplementedException();
+            return _context.Todos.AsNoTracking().Where(TodoQueries.GetAll(user)).OrderBy(x=>x.Date);
         }
 
         public IEnumerable<TodoItem> GetAllDone(string user)
@@ -42,7 +48,9 @@ namespace ToDo.Domain.Infra.Repositories
 
         public void Update(TodoItem todo)
         {
-            throw new NotImplementedException();
+            _context.Entry(todo).State = EntityState.Modified;
+            _context.SaveChanges();
         }
+    }
     }
 }

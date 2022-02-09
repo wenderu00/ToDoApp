0bcbe54f82eabbce63b9c1eaf1f99bc269b04727
wenderu00 <mwmcjr@gmail.com>
2022-02-08 21:55:03 -0300

    criação de teste de handler, todos os testes vermelhos

---
+++ ToDo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Domain.Tests.HandlerTests
+{
+    [TestClass]
+    public class CreateTodoHandlerTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido_deve_interromper_execucao()
+        {
+            Assert.Fail();
+        }
+        [TestMethod]
+        public void Dado_um_comando_valido_criar_tarefa()
+        {
+            Assert.Fail();
+        }
+    }
+}

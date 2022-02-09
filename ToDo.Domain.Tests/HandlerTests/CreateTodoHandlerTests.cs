cad7577425f34bf404fe8cd089b7c2674f48c057
wenderu00 <mwmcjr@gmail.com>
2022-02-08 22:26:36 -0300

    finalização do teste do handler

--- ToDo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ ToDo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -13,16 +13,27 @@ namespace ToDo.Domain.Tests.HandlerTests
     [TestClass]
     public class CreateTodoHandlerTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Comando valido", "usuario valido", DateTime.Now);
+     
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+        private GenericCommandResult _result = new GenericCommandResult();
         [TestMethod]
         public void Dado_um_comando_invalido_deve_interromper_execucao()
         {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(new FakeTodoRepository);
+            
+            
+            _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Sucess,false);
         }
         [TestMethod]
         public void Dado_um_comando_valido_criar_tarefa()
         {
-            Assert.Fail();
+           
+            _result = (GenericCommandResult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Sucess, true);
+
         }
     }
 }
